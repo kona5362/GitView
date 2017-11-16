f10fb934bdf96ae7e1a095c91a047cbd1e158523
kona94 <kona94@sina.com>
2017-11-16 20:39:37 +0800

    after clone

--- app/src/main/java/com/kona/gitdemo/MainActivity.java
+++ app/src/main/java/com/kona/gitdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("ss", "onCreate: ");
         //////
+        /////克隆修改
     }
 }
